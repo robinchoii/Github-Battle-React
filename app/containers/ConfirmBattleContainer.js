e177fcea2b1bce95f88bc998a19badb090f2f576
robinchoii <robinchoii@gmail.com>
2017-02-17 15:30:27 -0800

    create ui for user details

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -34,11 +34,20 @@ var ConfirmBattleContainer = React.createClass({
   componentWillUnmount: function() {
     console.log('componentWillUnmount')
   },
+  handleInitiateBattle: function() {
+    this.contextTypes.router.push({
+      pathname: '/results',
+      state: {
+        playersInfo: this.state.playersInfo
+      }
+    })
+  },
   render: function() {
     return (
       <ConfirmBattle
         isLoading={this.state.isLoading}
-        playersInfo={this.state.playersInfo} />
+        playersInfo={this.state.playersInfo}
+        onInitiateBattle={this.handleInitiateBattle} />
     )
   }
 });
